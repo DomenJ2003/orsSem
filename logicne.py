b0dd5fc5d23996dafa889f85b907f218ef7ee144
DomenJ2003 <domen.jakopic2@gmail.com>
2022-12-24 18:02:24 +0100

    se nekaj sprememb na logicnih

--- logicne.py
+++ logicne.py
@@ -4,73 +4,78 @@ import pretvorbe as p
 fontTitle = ("Moderen", 24)
 fontText = ("Moderen", 14)
 
+
 def orF(st1, st2):
     rezultat = ""
     for i in range(len(st1)):
-        if st1[i]=="1" or st2[i]=="1":
-            rezultat += "1" 
+        if st1[i] == "1" or st2[i] == "1":
+            rezultat += "1"
         else:
             rezultat += "0"
     return rezultat
+
 
 def norF(st1, st2):
     rezultat = ""
     for i in range(len(st1)):
-        if st1[i] == 1 or st2[i]=="1":
-            rezultat += "0" 
+        if st1[i] == "1" or st2[i] == "1":
+            rezultat += "0"
         else:
             rezultat += "1"
     return rezultat
+
 
 def andF(st1, st2):
     rezultat = ""
     for i in range(len(st1)):
-        if st1[i]=="1" and st2[i]=="1":
-            rezultat += "1" 
+        if st1[i] == "1" and st2[i] == "1":
+            rezultat += "1"
         else:
             rezultat += "0"
     return rezultat
+
 
 def nandF(st1, st2):
     rezultat = ""
     for i in range(len(st1)):
-        if st1[i]=="1" and st2[i]=="1":
-            rezultat += "0" 
+        if st1[i] == "1" and st2[i] == "1":
+            rezultat += "0"
         else:
             rezultat += "1"
     return rezultat
+
 
 def xorF(st1, st2):
     rezultat = ""
     for i in range(len(st1)):
-        if st1[i]=="1" and st2[i]=="0":
+        if st1[i] == "1" and st2[i] == "0":
             rezultat += "1"
-        elif st1[i]=="0" and st2[i]=="1":
+        elif st1[i] == "0" and st2[i] == "1":
             rezultat += "1"
         else:
-            rezultat += "0" 
+            rezultat += "0"
     return rezultat
+
 
 def xandF(st1, st2):
     rezultat = ""
     for i in range(len(st1)):
-        if st1[i]=="1" and st2[i]=="0":
+        if st1[i] == "1" and st2[i] == "0":
             rezultat += "0"
-        elif st1[i]=="0" and st2[i]=="1":
+        elif st1[i] == "0" and st2[i] == "1":
             rezultat += "0"
         else:
-            rezultat += "1" 
+            rezultat += "1"
     return rezultat
-
 
 
 def izracun(stevilo1, stevilo2, sestav1, sestav2, logOperator, izpis):
     if sestav1 == 'DEC':
-        stevilo1 = p.decToBin(int(stevilo1))        
+        stevilo1 = p.decToBin(int(stevilo1))
     elif sestav1 == 'OCT':
-        stevilo1 = p.octToBin(list(stevilo1))   
+        stevilo1 = p.octToBin(list(stevilo1))
     elif sestav1 == 'HEX':
-        stevilo1 = p.hexToBin(stevilo1)  
+        stevilo1 = p.hexToBin(stevilo1)
 
     if sestav2 == 'DEC':
         stevilo2 = p.decToBin(int(stevilo2))
@@ -79,18 +84,12 @@ def izracun(stevilo1, stevilo2, sestav1, sestav2, logOperator, izpis):
     elif sestav2 == 'HEX':
         stevilo2 = p.hexToBin(stevilo2)
 
-    if (len(stevilo1)-len(stevilo2))<0:
+    if (len(stevilo1)-len(stevilo2)) < 0:
         for i in range((len(stevilo2)-len(stevilo1))):
             stevilo1 = "0"+stevilo1
-    elif (len(stevilo1)-len(stevilo2))>0:
+    elif (len(stevilo1)-len(stevilo2)) > 0:
         for i in range(len(stevilo1)-len(stevilo2)):
             stevilo2 = "0"+stevilo2
-
-
-    print ("stevilo1: ")
-    print (stevilo1)
-    print ("stevilo2: ")
-    print (stevilo2)
 
     if logOperator == "OR":
         izpis.config(text=orF(stevilo1, stevilo2))
@@ -104,8 +103,7 @@ def izracun(stevilo1, stevilo2, sestav1, sestav2, logOperator, izpis):
         izpis.config(text=xorF(stevilo1, stevilo2))
     elif logOperator == "XAND":
         izpis.config(text=xandF(stevilo1, stevilo2))
-    
-    
+
 
 def layout(window):
     options = [
@@ -115,7 +113,6 @@ def layout(window):
         "HEX",
     ]
     optionsFunction = [
-        "NOT",
         "OR",
         "AND",
         "NAND",
@@ -124,7 +121,7 @@ def layout(window):
         "XAND"
     ]
     logOperacija = StringVar()
-    logOperacija.set("NOT")
+    logOperacija.set("OR")
 
     frame = Frame(window)
     Label(frame, text="Logične funkcije", font=fontTitle).pack()
@@ -132,17 +129,16 @@ def layout(window):
     line1 = Frame(frame)
     vpis1 = Entry(line1)
     vpis1.insert(0, '')
-    
 
     sestav1 = StringVar()
     sestav1.set("DEC")
-    drop1 = OptionMenu(line1 , sestav1 , *options )
+    drop1 = OptionMenu(line1, sestav1, *options)
     drop1.grid(row=1, column=2)
     vpis1.grid(row=1, column=1)
 
     line1.pack()
     Frame(frame, height=20).pack()
-    dropFunc = OptionMenu(frame , logOperacija , *optionsFunction )
+    dropFunc = OptionMenu(frame, logOperacija, *optionsFunction)
     dropFunc.pack()
     Frame(frame, height=20).pack()
     line2 = Frame(frame)
@@ -150,12 +146,12 @@ def layout(window):
     vpis2.insert(0, '')
     sestav2 = StringVar()
     sestav2.set("DEC")
-    drop2 = OptionMenu(line2 , sestav2 , *options )
+    drop2 = OptionMenu(line2, sestav2, *options)
     drop2.grid(row=1, column=2)
     vpis2.grid(row=1, column=1)
     line2.pack()
     Frame(frame, height=20).pack()
-    Button(frame, text="=", command=lambda:izracun(vpis1.get(), vpis2.get(), sestav1.get(), sestav2.get(), logOperacija.get(), rezultat)).pack() 
+    Button(frame, text="=", command=lambda: izracun(vpis1.get(), vpis2.get(), sestav1.get(), sestav2.get(), logOperacija.get(), rezultat)).pack()
     Frame(frame, height=20).pack()
     rezultat = Label(frame, text="")
     rezultat.pack()
